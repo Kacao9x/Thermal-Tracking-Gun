301bc6a076e959a5cb128dc7bcee1735b251e795
Kacao Cao <kacao@iastate.edu>
2018-03-24 04:03:12 -0400

    Refactor the program into modular design. Need to refactor the FIRE and the horizonal movement part

--- Thermal-tracking-gun/Thermal-tracking-gun.ino
+++ Thermal-tracking-gun/Thermal-tracking-gun.ino
@@ -1,5 +1,5 @@
-unsigned int yp = 0;
-unsigned int yn = 0;
+unsigned int y_p = 0;
+unsigned int y_n = 0;
 unsigned int x_L = 0;
 unsigned int x_R = 0;
 unsigned int i = 0, j = 0;         //index
@@ -41,112 +41,111 @@ void setup() {
     
 }
 
+
 void loop() {
-      FIRE = false;
-      yp = 0;
-      yn = 0;
-      
-      if(intReceived){
-            //get which pixels triggered
-            amg.getInterrupt(pixelInts);
+    FIRE = false;
+    y_p = 0;
+    y_n = 0;
+    
+    if(intReceived){
+        //get which pixels triggered
+        amg.getInterrupt(pixelInts);
 
-            Serial.println("**** interrupt received! ****");
-//            for(int i=0; i<8; i++){
-//                Serial.println(pixelInts[i], BIN);
-//            }
-//            Serial.println();
+        Serial.println("**** interrupt received! ****");
 
-            /* ------- VERTICAL: Algorithm to calculate the sum of bottom half and top half */
-//            yp = 0;
-//            yn = 0;
-            for(i = 0; i < 4; i++){
-                yp += pixelInts[i];
-            }
-            for(i = 4; i<8; i++){
-                yn += pixelInts[i];
-            }
+        /* ------- VERTICAL: Algorithm to calculate the sum of bottom half and top half */
+        //y_p = 0;
+        //y_n = 0;
+        sum_bottom_and_top_half();
+        if(y_n <  y_p) {
+            Serial. println("move up");
+            // TO-DO: move the servo
+        } else {
+            Serial.println("move down");
+            // TO-DO: move the servo
+        }
 
-            if(yn <  yp) 
-            {
-               Serial. println("move up");
-               // TO-DO: move the servo
-            }
-            else
-            {
-                Serial.println("move down");
-                // TO-DO: move the servo
-            }
-
-            /* ------ Finished Calculation -------- */
+        /* ------ Finished Calculation -------- */
 
 
-            /* ------- HORIZONTAL: Algorithm to calculate the sum of left half and right half */
-            x_L = 0;
-            x_R = 0;
-            int value = 0;
-            
-            for(i=0; i<8; i++) {
-                value = pixelInts[i];
-                //Serial.print("value of 8bit array: "); Serial.println(value);       //print out octal value
+        /* ------- HORIZONTAL: Algorithm to calculate the sum of left half and right half */
+        x_L = 0;
+        x_R = 0;
+        int value = 0;
+        
+        for(i=0; i<8; i++) {
+            value = pixelInts[i];
+            //Serial.print("value of 8bit array: "); Serial.println(value);       //print out octal value
 
-                /* calculate the right 4 bits */
-                 for (j=0; j<4; j++) {
-                    if (value % 2 == 1) {
-                        x_R +=1;
-                        if ( (j == 3 && i == 3) || (j == 3 && i == 4) ) {
-                            FIRE = true;
-                        }
-//                        value >> 1;             // divide by 2: not working bc value is int value
-                        value /= 2;
-                    } else {
-//                        value >> 1;             //divide by 2
-                        value /= 2;
+            /* calculate the right 4 bits */
+            for (j=0; j<4; j++) {
+                if (value % 2 == 1) {
+                    x_R += 1;
+                    /* check if the central bit value is 1 */
+                    if ( (j == 3 && i == 3) || (j == 3 && i == 4) ) {
+                        FIRE = true;
                     }
-                } /* --- END of the right 4-bit interation --- */
-
-                /* calculate the left 4 bits */
-                 for (j=0; j<4; j++) {
-                    if (value % 2 == 1) {
-                        x_L +=1;
-                        if( (j == 0 && i == 3) || (j == 0 && i == 4) ) {
-                            FIRE = true;
-                        }
-//                        value >> 1;
-                        value /= 2;
-                    } else {
-//                        value >> 1;
-                        value /= 2;
-                    }
-                }/* --- END of the left 4-bit interation --- */
-            }   /* --- END of the row interation --- */
-
-                if (x_L < x_R) {
-                    Serial.println("move Right");
-                    //move right;
+                    //value >> 1; // divide by 2: not working b/c value is int value
+                    value /= 2;
                 } else {
-                    Serial.println("move Left");
-                    //move left;
+                    value /= 2;
                 }
-            
-           
-          
+            } /* --- END of the right 4-bit interation --- */
 
-            Serial.println();
-            if (FIRE == true ) {
-                Serial.println("FIREEEEE");
+            /* calculate the left 4 bits */
+            for (j=0; j<4; j++) {
+                if (value % 2 == 1) {
+                    x_L +=1;
+                    /* check if the central bit value is 1 */
+                    if( (j == 0 && i == 3) || (j == 0 && i == 4) ) {
+                        FIRE = true;
+                    }
+                    value /= 2;
+                } else {
+                    value /= 2;
+                }
+            }/* --- END of the left 4-bit interation --- */
+        }   /* --- END of the row interation --- */
+
+            if (x_L < x_R) {
+                Serial.println("move Right");
+                //move right;
             } else {
-                Serial.println("Checkkking tartget");
+                Serial.println("move Left");
+                //move left;
             }
-            
-            Serial.println();
+        
+        Serial.println();
+        if (FIRE == true ) {
+            Serial.println("FIREEEEE");
+            //gun_fire();
+        } else {
+            Serial.println("Checkkking tartget");
+        }
+        
+        Serial.println();
 
-            /* ------ Finished Calculation -------- */
-            
-            //clear the interrupt so we can get the next one!
-            amg.clearInterrupt();
-            
-            intReceived = false;
-      }
+        /* ------ Finished Calculation -------- */
+        
+        //clear the interrupt so we can get the next one!
+        amg.clearInterrupt();       // clear Interrupt
+        intReceived = false;        // reset the interrupt flag
+    }
+
+}
+
+void sum_bottom_and_top_half() {
+//            y_p = 0;
+//            y_n = 0;
+    for(i = 0; i < 4; i++) {
+        y_p += pixelInts[i];
+    }
+    for(i = 4; i<8; i++) {
+        y_n += pixelInts[i];
+    }
+}
+
+void gun_fire() {
 
 }
 
